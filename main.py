1993786e44e2470362b1a9aba73c1f7d0f26a162
kaushik kochhar <kaushikkochhar@MacBook-Pro.local>
2022-12-27 14:10:03 +0530

    test

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ appliances=st.multiselect("Choose appliances",['Air Conditioner','CF Bulb','Halo
 st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
 with st.expander("See explanation"):
-    st.write(
-        The chart above shows some numbers I picked for you.
-        I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-    )
+    st.write('\
+        The chart above shows some numbers I picked for you. \
+        I rolled actual dice for these, so they\'re *guaranteed* to \
+        be random. \
+    ')
     st.image("https://static.streamlit.io/examples/dice.jpg")
 
 #st.write(f'Hello {name}!')
