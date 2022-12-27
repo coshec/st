df740fd8892073bcf3128bc96627a7bc61632937
kaushik kochhar <kaushikkochhar@MacBook-Pro.local>
2022-12-27 14:15:53 +0530

    test

--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import pandas as pd
 st.title('Diesel Generator Size Calculator')
 #name = st.text_input('whats your name', '')
 appliances=st.multiselect("Choose appliances",['Air Conditioner','CF Bulb','Halogen light','Motor'],['CF Bulb'])
-st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 
-with st.expander("See explanation"):
-    st.write('\
-        The chart above shows some numbers I picked for you. \
-        I rolled actual dice for these, so they\'re *guaranteed* to \
-        be random. \
-    ')
-    st.image("https://static.streamlit.io/examples/dice.jpg")
-
+if len(appliances)> 1:
+    tabs=st.tabs(appliances)
 #st.write(f'Hello {name}!')
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
